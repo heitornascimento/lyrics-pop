11b484ce377427a1ac852bb2a1553281656abf05
Carlos Eduardo Jorge <carlos.jorge@sumup.com>
2019-01-17 11:09:03 -0200

    adding questions and returning json

--- back/app/controllers/quests_controller.rb
+++ back/app/controllers/quests_controller.rb
@@ -1,10 +1,77 @@
 class QuestsController < ApplicationController
   before_action :set_quest, only: [:show, :edit, :update, :destroy]
 
+  QUESTS =
+    [
+      {
+        id: 0,
+        description: "Samba, na cara da inimiga vai\nSamba, desfila com as amiga vai\nSamba, na cara da inimiga vai\nSamba, desfila com as amiga",
+        answers: [
+          'Zeca pagodinho',
+          'Jojô Toddynho',
+          'Tom Zé',
+          'Valesca Popozuda'
+        ],
+        correct_answer: 1
+      },
+
+      {
+       id: 1,
+       description: "Amar, amei\n
+                     Gostar, gostei\n
+                     Mas agora eu não quero\n
+                     Nem de graça",
+       answers: [
+         'MC Jhonatan',
+         'MC Brinquedo',
+         'MC Loma e As Gêmeas Lacração',
+         'Mc Don Juan'
+       ],
+       correct_answer: 3
+      },
+
+      {
+        id: 2,
+        description: "Punhos cerrados, olhos fechados\nE eu levanto a mão pro alto e grito\nVem comigo quem é do bonde pesadão!",
+        answers: [
+          'Sofia',
+          'Iza',
+          'Ingrid',
+          'Mariana'
+        ],
+        correct_answer: 1
+      },
+
+      {
+        id: 3,
+        description: "We're no strangers to love\nYou know the rules and so do I\nA full commitment's what I'm thinking of\nYou wouldn't get this from any other guy",
+        answers: [
+          'Justin Bieber',
+          'Michael Jackson',
+          'The Killers',
+          'Rick Astley'
+        ],
+        correct_answer: 3
+      },
+      {
+        id: 4,
+        description: "E aê, DG?\nEscama só de peixe\nUaai!\nCebruthius",
+        answers: [
+          'MC Loma e As Gêmeas Lacração',
+          'MC Bruthius',
+          'Furacão 2000',
+          'MC Henrique'
+        ],
+        correct_answer: 0
+      }
+    ]
+
   # GET /quests
   # GET /quests.json
   def index
-    @quests = Quest.all
+    respond_to do |format|
+      format.json { render json: QUESTS }
+    end
   end
 
   # GET /quests/1
