6099e49837d73fa22665cf42c7d91bdf7b3f75b2
Giorgio Torres <giorgio.torres@sumup.com>
2019-01-17 15:37:47 -0200

    Migration for database population and refactor of index

--- back/app/controllers/game_controller.rb
+++ back/app/controllers/game_controller.rb
@@ -1,10 +1,7 @@
 class GameController < ApplicationController
 
   def index
-    @quest = Quest.new
-    @quest.question = "Bla"
-    @quest.options = "foo1?;foo2?;foo3?"
-    @quest.answer_idx = 1
+    @quest = Quest.all.sample
   end
 
 end
